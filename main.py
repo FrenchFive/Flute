0864ed3346954bf4b35c9b78cba156cfeb156396
FIVE <cahhong@artfx.fr>
2025-07-19 23:38:32 +0100

    Switch to Pan Flute soundfont and improve audio processing
    Replaced multiple flute soundfonts with a single Pan_Flute.sf2 and updated main.py to use it. Added MIDI cleaning to remove low-velocity and low-pitch notes, increased audio gain, and applied loudness normalization to MP3 conversion. Updated ytb.py to read the YouTube URL from ytb.txt if available, and modified .gitignore to exclude ytb.txt.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from basic_pitch.inference import predict_and_save
 from basic_pitch import ICASSP_2022_MODEL_PATH
 import subprocess
 import os
+import mido
 
 
 script_path = os.path.dirname(os.path.abspath(__file__))
@@ -17,6 +18,18 @@ predict_and_save(
     ICASSP_2022_MODEL_PATH    # 7) model path
 )
 
+def clean_midi(midi_path):
+    mid = mido.MidiFile(midi_path)
+    for track in mid.tracks:
+        for msg in track:
+            #remove if velocity is below 10
+            if msg.type == 'note_on' and msg.velocity < 5:
+                msg.velocity = 0
+            #remove if note is too low pitch
+            if msg.type == 'note_on' and msg.note < 10:
+                msg.velocity = 0
+    mid.save(midi_path)
+
 
 def convert_midi_to_audio(midi_path, soundfont_path, wav_path):
     subprocess.run([
@@ -24,6 +37,7 @@ def convert_midi_to_audio(midi_path, soundfont_path, wav_path):
         "-ni",
         "-F", wav_path,
         "-r", "44100",
+        "-g", "4.0",
         soundfont_path,
         midi_path
     ])
@@ -32,6 +46,7 @@ def convert_wav_to_mp3(wav_path, mp3_path):
     subprocess.run([
         "ffmpeg",
         "-i", wav_path,
+        "-filter:a", "loudnorm",
         "-codec:a", "libmp3lame",
         "-qscale:a", "2",
         mp3_path
@@ -40,15 +55,18 @@ def convert_wav_to_mp3(wav_path, mp3_path):
 midi_path = os.path.join(script_path, "outputs", "audio_basic_pitch.mid")
 wav_path = os.path.join(script_path, "final.wav")
 
+clean_midi(midi_path)
+
 convert_midi_to_audio(
     midi_path,
-    os.path.join(script_path, "soundfont", "flute4.sf2"),
+    os.path.join(script_path, "soundfont", "Pan_Flute.sf2"),
     wav_path
 )
 
-os.remove(midi_path)
-
 mp3_path = os.path.join(script_path, "final.mp3")
+os.remove(midi_path)
+os.remove(mp3_path)
+
 convert_wav_to_mp3(
     wav_path,
     mp3_path
--- ytb.py
+++ ytb.py
@@ -1,6 +1,15 @@
 import yt_dlp
+import os 
 
-url = "https://www.youtube.com/watch?v=YXwYJyrKK5A"
+file = "ytb.txt"
+if os.path.exists(file):
+    with open(file, "r") as f:
+        url = f.read().strip()
+else:
+    url = "https://youtu.be/Ta2ULGIi2yk?si=gB6H-d5REEg_Md3n"
+
+url = url.replace("&start_radio=1", "")
+
 
 ydl_opts = {
     'format': 'bestaudio/best',
